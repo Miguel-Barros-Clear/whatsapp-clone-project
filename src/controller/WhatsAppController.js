d68b0520be4ab1bb4fc925ae8201e950a4b2fd5c
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-14 12:09:12 -0300

    feat: adding functions to attach documents

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import { Format } from "../util/Format";
 import { CameraController } from "./CameraController";
+import { DocumentPreviewController } from "./DocumentPreviewController";
 
 export class WhatsAppController {
     constructor() {
@@ -182,10 +183,59 @@ export class WhatsAppController {
 
         this.el.btnAttachDocument.on('click', (e) => {
             this.closeAllMainPanel();
-            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelCamera.hide();
+            this.el.panelDocumentPreview.show();
             this.el.panelDocumentPreview.css({
-                'height': 'calc(100%)'
+                'height': 'calc(100%)',
+                'transform': 'translateY(0px)'
             })
+            this.el.inputDocument.click();
+        })
+
+        this.el.inputDocument.on('change', (e) => {
+            if (this.el.inputDocument.files.length) {
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then((result) => {
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch((err) => {
+
+                    switch (file.type) {
+                        case 'application/vnd.ms-excel':
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls'
+
+                            break;
+
+                        case 'application/vnd.ms-powerpoint':
+                        case 'application/vnd.openxmlformats-offiecedocument.presentationml.presentation':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt'
+
+                            break;
+
+
+                        case 'application/msword':
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc'
+
+                            break;
+
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic'
+                            break;
+                    }
+
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+                })
+            }
         })
 
         this.el.btnClosePanelDocumentPreview.on('click', (e) => {
@@ -299,7 +349,7 @@ export class WhatsAppController {
     closeAllMainPanel() {
         this.el.panelMessagesContainer.hide();
         this.el.panelDocumentPreview.hide();
-        this.el.panelCamera.removeClass('open')
+        this.el.panelCamera.removeClass('open');
     }
 
     closeMenuAttach(e) {
